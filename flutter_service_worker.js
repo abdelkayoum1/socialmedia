ce3b46daaedd67bbbaaadea9cd2265a68f7b80ef
abdelkayoum <abdelkayoumkh0@gmail.com>
2026-04-27 12:09:47 +0100

    modifier

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "84eb22643ac273ea328d4c6f7f19badb
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "56b9fb65d451f75f18bc17a5382307ba",
+"flutter_bootstrap.js": "d92bb8f225dabfe0803a150d154fcec5",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
